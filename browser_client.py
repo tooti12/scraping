ccac613cd2a3f6095ad3eb076497b94525415831
umar <umarjaved56@gmail.com>
2025-08-31 02:11:03 +0100

    add submit button

--- browser_client.py
+++ browser_client.py
@@ -39,6 +39,7 @@ class BrowserClient:
     def solve_captcha(self):
         try:
             self.sb.uc_gui_click_captcha()
+            self.click('button:contains("Submit")')
             # self.sb.driver.uc_click("button.mat-btn-lg")
         except:
             print("Could not solve catpcha")
